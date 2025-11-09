41492e3f7b8de34e632b5d58e330beff7accbbf5
Banglarboy-lab <sazzadul2018@gmail.com>
2025-11-10 01:29:07 +0600

    Initial commit

---
+++ public/main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.remove('opacity-0', 'translate-y-8');
+        entry.target.classList.add('opacity-100', 'translate-y-0');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 0.2 });
+
+  document.querySelectorAll('[data-animate-on-scroll]').forEach(el => {
+    observer.observe(el);
+  });
+});
